c3c7cc8bed9e673911bb476820f42b2f68b22b05
Skylar <Skylar.beacham@outlook.com>
2021-05-16 11:48:42 +0100

    tidied code

--- flashie.js
+++ flashie.js
@@ -2,18 +2,21 @@ const {Client} = require('discord.js');
 const client = new Client();
 require('dotenv').config();
 
-process.on('unhandledRejection', async () =>{
-    console.log("error with promise");
-});
-
+// process.on('unhandledRejection', async () =>{
+//     console.log("error with promise");
+// });
 client.on('ready', async () =>{
-    await console.log("Bot: Ready");
+    await console.log(`${client.user.username} AD: Ready`);
 });
-
+    
 client.on('message', async (message) =>{
-    if(message.channel.name.toLowerCase().includes("auto-delete")) await message.delete({timeout: 60000}).catch(async error => {
-        await console.log("Error: Message Not available to delete!")
-    });
+    //General
+    if(message.channel.name.toLowerCase().includes("auto-delete")){
+        await message.delete({timeout: 6000}).catch(async error => {
+            await console.log("Error: Message Not available to delete!")
+        });
+    } 
 })
+
 
 client.login(process.env.Token);
